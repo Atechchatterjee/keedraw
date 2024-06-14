d408071630537f3438893ba68686902b6663872f
Atechchatterjee <atechchatterjeee@gmail.com>
2024-06-14 22:56:06 +0530

    feat: defined types for graph node

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,1 +1,12 @@
 export type MODE = "normal" | "insert" | "movement" | "visual";
+export type DIR = "tl" | "bl" | "l" | "r" | "tr" | "br" | "t" | "b" | "";
+
+export interface GraphNode {
+  id: string;
+  x: number;
+  y: number;
+  text: string;
+  length: number;
+  children: GraphNode[];
+  dir?: DIR;
+}
