b7527c7fe9b3f763792f8642eff17c13895e5e5b
Atechchatterjee <atechchatterjeee@gmail.com>
2024-06-15 21:56:45 +0530

    feat: add parentId & change children to adj list (GraphNode)

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,12 +1,16 @@
 export type MODE = "normal" | "insert" | "movement" | "visual";
 export type DIR = "tl" | "bl" | "l" | "r" | "tr" | "br" | "t" | "b" | "";
 
+// GraphMapId = "x,y_GraphNode.id"
+export type GraphMapId = `${number},${number}_${string}`;
+
 export interface GraphNode {
   id: string;
   x: number;
   y: number;
   text: string;
   length: number;
-  children: GraphNode[];
-  dir?: DIR;
+  adj: Map<DIR, GraphMapId>;
+  parentId: string;
+  dir?: DIR; // direction w.r.t to the parent
 }
