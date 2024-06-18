635a9ff3777363adc4088e803b75981159dd568a
Atechchatterjee <atechchatterjeee@gmail.com>
2024-06-18 22:02:37 +0530

    refactor: remove a duplicate function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,9 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function generateGraphIds(): string {
-  const dateString = Date.now().toString(36);
-  const randomness = Math.random().toString(36).substring(2);
-  return dateString + randomness;
-}
